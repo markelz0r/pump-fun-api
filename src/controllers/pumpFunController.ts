c083d61700f9cacb368ae3ad23c50a4efd39f865
Andrew Odintsov <a1covker@gmail.com>
2024-12-06 22:16:42 +0000

    Add more safety to the controller

--- src/controllers/pumpFunController.ts
+++ src/controllers/pumpFunController.ts
@@ -14,21 +14,28 @@ type MintResponse = {
 }
 
 export async function getMint(req: Request, res: Response) {
-    const { mint } = req.params;
+    try {
+        const { mint } = req.params;
 
-    const coin: PumpCoin = await PumpCoinModel.findOne({ mint: mint }).lean();
-    const transactions: PumpTransaction[] = await PumpTransactionModel.find({curve: coin.curve}).lean();
+        const coin: PumpCoin = await PumpCoinModel.findOne({ mint: mint }).lean();
+        const transactions: PumpTransaction[] = await PumpTransactionModel.find({curve: coin.curve}).lean();
 
 
-    const response: MintResponse = {
-        mint: mint,
-        avgPrice: average(transactions, 'price'),
-        avgQuantity: average(transactions, 'quantity'),
-        priceVolatility: stdDev(transactions, 'price'),
-        totalVolume: sum(transactions, 'quantity'),
-        momentum: momentum(transactions),
-        transactions: transactions.length,
-    };
+        const response: MintResponse = {
+            mint: mint,
+            avgPrice: average(transactions, 'price'),
+            avgQuantity: average(transactions, 'quantity'),
+            priceVolatility: stdDev(transactions, 'price'),
+            totalVolume: sum(transactions, 'quantity'),
+            momentum: momentum(transactions),
+            transactions: transactions.length,
+        };
 
-    res.json(response)
+        res.json(response)
+    }
+    catch (e) {
+        console.log(e)
+        res.status(500)
+    }
+
 }
